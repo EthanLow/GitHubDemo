ae7e6661849261f5c7457d0f05e7624404522a9e
EthanLow <liuleiailin@sina.com>
2017-05-23 15:34:51 +0800

    gitHubDemo

---
+++ githubDemo/githubDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubDemo
+//
+//  Created by ethanlow on 17/5/23.
+//  Copyright © 2017年 ethanlow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubDemo/githubDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubDemo
+//
+//  Created by ethanlow on 17/5/23.
+//  Copyright © 2017年 ethanlow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
